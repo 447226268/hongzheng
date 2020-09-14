5bbc57a06474da57346bb2c2ac77aade560d0046
liude950103 <liude950103@163.com>
2020-09-15 04:23:30 +0800

    主页

---
+++ web/src/views/home copy.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="orderinfo">
+    <div class="table">
+      <div class="ordertablet">
+        <el-table :data="tableData" style="width: 100%">
+          <el-table-column type="expand">
+            <template slot-scope="props">
+              <el-form label-position="left" inline class="demo-table-expand">
+                <el-form-item label="意向程度">
+                  <span>{{ props.row.degree }}</span>
+                </el-form-item>
+                <el-form-item label="跟踪状态">
+                  <span>{{ props.row.state  }}</span>
+                </el-form-item>
+              </el-form>
+            </template>
+          </el-table-column>
+          <el-table-column label="编号" align='center' prop="id"></el-table-column>
+          <el-table-column label="性别" align='center' prop="gender"></el-table-column>
+          <el-table-column label="生日" align='center' prop="birthday"></el-table-column>
+          <el-table-column label="手机号" align='center' prop="telephone"></el-table-column>
+          <el-table-column label="咨询校区" align='center' prop="room.name"></el-table-column>
+          <!-- <el-table-column label="跟踪状态" align='center'>
+            <template slot-scope="scope">
+              <el-tag :type="scope.row.state === '正常' ? 'success' : 'danger'" effect="dark">{{scope.row.state}}</el-tag>
+            </template>
+          </el-table-column>
+          <el-table-column label="意向程度" align='center' prop="degree"></el-table-column> -->
+          <el-table-column label="录入人" align='center' prop="handler"></el-table-column>
+          <el-table-column label="录入时间" align='center' prop="date"></el-table-column>
+          <el-table-column label="线索" align='center' prop="info"></el-table-column>
+          <el-table-column label="是否试听" align='center' prop="islisten"></el-table-column>
+        </el-table>
+      </div>
+      <el-pagination class="pagination" background layout="prev, pager, next" :total="total" :current-page="current"
+        @current-change="currentChange">
+      </el-pagination>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'home',
+    data() {
+      return {
+        current: 1,
+        total: 1000,
+        size: 10,
+        tableData: [],
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .orderinfo {
+
+    .table {
+      padding: 60px 24px 24px 24px;
+      background-color: #fff;
+
+      .pagination {
+        margin-top: 40px;
+        margin-bottom: 20px;
+        text-align: center;
+      }
+    }
+
+    .ordertablet {
+      .demo-table-expand {
+        font-size: 0;
+      }
+
+      .demo-table-expand label {
+        width: 90px;
+        color: #99a9bf;
+      }
+
+      .demo-table-expand .el-form-item {
+        margin-right: 0;
+        margin-bottom: 0;
+        width: 50%;
+      }
+    }
+  }
+</style>
--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -1,87 +1,161 @@
 <template>
-  <div class="orderinfo">
-    <div class="table">
-      <div class="ordertablet">
-        <el-table :data="tableData" style="width: 100%">
-          <el-table-column type="expand">
-            <template slot-scope="props">
-              <el-form label-position="left" inline class="demo-table-expand">
-                <el-form-item label="意向程度">
-                  <span>{{ props.row.degree }}</span>
-                </el-form-item>
-                <el-form-item label="跟踪状态">
-                  <span>{{ props.row.state  }}</span>
-                </el-form-item>
-              </el-form>
-            </template>
-          </el-table-column>
-          <el-table-column label="编号" align='center' prop="id"></el-table-column>
-          <el-table-column label="性别" align='center' prop="gender"></el-table-column>
-          <el-table-column label="生日" align='center' prop="birthday"></el-table-column>
-          <el-table-column label="手机号" align='center' prop="telephone"></el-table-column>
-          <el-table-column label="咨询校区" align='center' prop="room.name"></el-table-column>
-          <!-- <el-table-column label="跟踪状态" align='center'>
-            <template slot-scope="scope">
-              <el-tag :type="scope.row.state === '正常' ? 'success' : 'danger'" effect="dark">{{scope.row.state}}</el-tag>
-            </template>
-          </el-table-column>
-          <el-table-column label="意向程度" align='center' prop="degree"></el-table-column> -->
-          <el-table-column label="录入人" align='center' prop="handler"></el-table-column>
-          <el-table-column label="录入时间" align='center' prop="date"></el-table-column>
-          <el-table-column label="线索" align='center' prop="info"></el-table-column>
-          <el-table-column label="是否试听" align='center' prop="islisten"></el-table-column>
-        </el-table>
-      </div>
-      <el-pagination class="pagination" background layout="prev, pager, next" :total="total" :current-page="current"
-        @current-change="currentChange">
-      </el-pagination>
+  <div class="home">
+    <!-- 统计分析和下拉框 -->
+    <div class="show1">
+      <el-row :gutter="20" type="flex" justify="space-between">
+        <el-col>
+          <p>首页</p>
+        </el-col>
+        <el-col align="right">
+          <div>
+            <span>校区：</span>
+            <el-select v-model="value" :placeholder="roomList[0].name" @change="changeRoomId">
+              <el-option
+                v-for="item in roomList"
+                :key="item.id"
+                :label="item.name"
+                :value="item.id"
+              ></el-option>
+            </el-select>
+          </div>
+        </el-col>
+      </el-row>
+    </div>
+
+    <!-- 信息模块 -->
+    <div class="show2">
+      <el-row :gutter="20" justify="space-between">
+        <el-col :span="10">
+          <!-- 今日数据变动 -->
+          <div class="info-change">
+            <div class="normal-box">
+              <i class="el-icon-s-data"></i>
+              <span class="info-change-title">今日数据变动</span>
+            </div>
+          </div>
+        </el-col>
+
+        <el-col :span="8">
+          <!-- 事项提醒 -->
+          <div class="info-change">
+            <div class="normal-box">
+              <i class="el-icon-message-solid"></i>
+              <span class="info-change-title">事项提醒</span>
+            </div>
+          </div>
+        </el-col>
+
+        <el-col :span="6">
+          <!-- 生日学员 -->
+          <div class="info-change">
+            <div class="normal-box">
+              <i class="el-icon-date"></i>
+              <span class="info-change-title">生日学员</span>
+            </div>
+          </div>
+        </el-col>
+
+        <el-col :span="24">
+          <!-- 今日课消 -->
+          <div class="info-change">
+            <div class="normal-box">
+              <i class="el-icon-reading"></i>
+              <span class="info-change-title">今日课消</span>
+            </div>
+          </div>
+        </el-col>
+
+        <el-col :span="24">
+          <!-- 今日课业绩 -->
+          <div class="info-change">
+            <div class="normal-box">
+              <i class="el-icon-money"></i>
+              <span class="info-change-title">今日业绩</span>
+            </div>
+          </div>
+        </el-col>
+      </el-row>
     </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'home',
-    data() {
-      return {
-        current: 1,
-        total: 1000,
-        size: 10,
-        tableData: [],
-      }
-    }
-  }
+import { getRoomList } from "@/api/studentinfo.js";
+export default {
+  name: "home",
+  data() {
+    this.chartSettings = {
+      radius: [70, 90],
+      offsetY: 120,
+    };
+    this.barExtend = {
+      legend: {
+        type: "scroll",
+        orient: "vertical",
+        height: "auto",
+        top: 10,
+        bottom: 10,
+        right: 30,
+      },
+    };
+
+    return {
+      currentroomid: 1,
+      roomList: null,
+    };
+  },
+  created() {
+    this.getRoom();
+  },
+  methods: {
+    async getRoom() {
+      let result = (await getRoomList()).result;
+      this.roomList = result;
+      this.currentroomid = result[0].id;
+    },
+    changeRoomId(id) {
+      this.currentroomid = id;
+      console.log(this.currentroomid);
+    },
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-  .orderinfo {
+<style scoped lang="scss">
+.home {
+  overflow-x: hidden;
+  width: 100% !important;
+  box-sizing: border-box;
 
-    .table {
-      padding: 60px 24px 24px 24px;
-      background-color: #fff;
+  .el-row {
+    position: relative;
+    box-sizing: border-box;
+    box-sizing: border-box;
+    width: 100%;
+  }
 
-      .pagination {
-        margin-top: 40px;
-        margin-bottom: 20px;
-        text-align: center;
-      }
-    }
+  .normal-box {
+    padding: 16px 20px;
+    background-color: #fff;
+    height: 270px;
+  }
 
-    .ordertablet {
-      .demo-table-expand {
-        font-size: 0;
-      }
+  .show1 {
+    font-size: 14px;
+    margin-bottom: 20px;
+  }
 
-      .demo-table-expand label {
-        width: 90px;
-        color: #99a9bf;
-      }
+  .show2 {
+    width: 100%;
+    position: relative;
+    height: auto;
+    zoom: 1;
+    display: flex;
+    flex-flow: row wrap;
 
-      .demo-table-expand .el-form-item {
-        margin-right: 0;
-        margin-bottom: 0;
-        width: 50%;
-      }
+    .info-change {
+      margin-bottom: 24px;
     }
   }
+}
 </style>
